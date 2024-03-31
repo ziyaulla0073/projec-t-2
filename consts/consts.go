e39d0f373d696e89b1747b9963e7b9e9c76dad0c
ziyaulla0073 <164716031+ziyaulla0073@users.noreply.github.com>
2024-03-31 22:25:11 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ziyaulla"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ziya"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ZU"
 )
 
 var ID ids.ID
